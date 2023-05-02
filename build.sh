8876b873fb4933aea46de041908222539a3ce570
Craeckie <josia-login@rosskopfs.de>
2023-05-02 13:09:37 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 version="16.19.0"
 nvm use "$version"
-wget -O "/tmp/node-v$version-headers.tar.gz" "https://nodejs.org/download/release/v$version/node-v$version-headers.tar.gz"
+set -x
+dst_path="/tmp/node-v$version-headers.tar.gz"
+[[ ! -f "$dst_path" ]] && wget -O "$dst_path" "https://nodejs.org/download/release/v$version/node-v$version-headers.tar.gz"
 npm config set tarball /tmp/node-v16.19.0-headers.tar.gz
 npm install --force 
 npm run build
